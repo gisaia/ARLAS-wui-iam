83b920a39ecf3bc5c7a36438a5729c8a4b9dffcb
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2023-09-04 18:50:02 +0200

    feat: use toolkit 24.3.0-beta-iam.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,50 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { ArlasIamService, ArlasSettingsService, AuthentificationService, IamInterceptor, LoginModule } from 'arlas-wui-toolkit';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { AuthentificationService, LoginModule } from 'arlas-wui-toolkit';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { UserComponent } from './components/user/user.component';
-import { IamStartupService } from './services/startup/startup.service';
-import { RoleComponent } from './components/role/role.component';
-import { PermissionComponent } from './components/permission/permission.component';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TopMenuComponent } from './components/top-menu/top-menu.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { MatSelectModule } from '@angular/material/select';
-import { RoleFormComponent } from './components/role/role-form/role-form.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { PermissionCreateComponent } from './components/permission/permission-create/permission-create.component';
-import { UserFormComponent } from './components/user/user-form/user-form.component';
-import { UserAddComponent } from './components/user/user-add/user-add.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { ToastrModule } from 'ngx-toastr';
-import { RulesComponent } from './components/rules/rules.component';
-import { RulesItemComponent } from './components/rules-item/rules-item.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatSortModule } from '@angular/material/sort';
-import { MatCardModule } from '@angular/material/card';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatDialogModule } from '@angular/material/dialog';
 import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
+import { CreateOrgModalComponent } from './components/home/create-org-modal/create-org-modal.component';
+import { HomeComponent } from './components/home/home.component';
 import {
   PermissionCreateColumnFilterComponent
 } from './components/permission/permission-create-column-filter/permission-create-column-filter.component';
-import { CreateOrgModalComponent } from './components/home/create-org-modal/create-org-modal.component';
+import { PermissionCreateComponent } from './components/permission/permission-create/permission-create.component';
+import { PermissionComponent } from './components/permission/permission.component';
+import { RoleFormComponent } from './components/role/role-form/role-form.component';
+import { RoleComponent } from './components/role/role.component';
+import { RulesItemComponent } from './components/rules-item/rules-item.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { UserAddComponent } from './components/user/user-add/user-add.component';
+import { UserFormComponent } from './components/user/user-form/user-form.component';
+import { UserComponent } from './components/user/user.component';
+import { IamStartupService } from './services/startup/startup.service';
 
 export function startupServiceFactory(startup: IamStartupService) {
   const load = () => startup.load();
@@ -128,12 +128,6 @@ export function createTranslateLoader(http: HttpClient) {
       provide: 'AuthentificationService',
       useFactory: auhtentServiceFactory,
       deps: [AuthentificationService],
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: IamInterceptor,
-      deps: [ArlasIamService, ArlasSettingsService],
       multi: true
     }
   ],
